449b09efdee8fe430ff12b090d049d7de94c14c6
Jarod <gpsebernicke@gmail.com>
2014-05-01 19:21:16 +0200

    Added Static Desktop referenz

--- SchoolOSX/include/Data.h
+++ SchoolOSX/include/Data.h
@@ -1,16 +1,11 @@
+#include "xcb/xcb.h"
+#include "stdint.h"
 #ifndef DATA
 #define DATA
-#include "xcb/xcb.h"
-#include "xcb/xcb_atom.h"
-#include "xcb_icccm.h"
-#include "xcb/xproto.h"
-#include "xcb/xcb_util.h"
-#include "stdint.h"
 using namespace std;
-
-xcb_connection_t *connection;
-
-
+class Desktop;
+extern xcb_connection_t* connection;
+extern Desktop* desktop;
 #endif
 
 
